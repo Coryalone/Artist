fa89734b8d36bf926a0808a5fa444ec52500af18
Balaraphon <Balaraphon@yandex.ru>
2022-07-04 15:34:35 +0300

    Настроил джанго для хероки

--- Artist/Artist/wsgi.py
+++ Artist/Artist/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Artist.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root="/Artist/main/static/")
+
+
